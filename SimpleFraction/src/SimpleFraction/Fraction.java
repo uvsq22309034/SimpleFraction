f468b5da9bf89baaf9e75e46f3fd435a0e2028dd
Zagoum Carnot <zagoumcarnot@gmail.com>
2024-09-25 18:55:37 +0200

    ajout de la methode tostring

--- SimpleFraction/src/SimpleFraction/Fraction.java
+++ SimpleFraction/src/SimpleFraction/Fraction.java
@@ -1,5 +1,22 @@
 package SimpleFraction;
 
 public class Fraction {
+	
+	private int numerator;
+    private int denominator;
+
+    // Constructeur
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+ // Représentation en chaîne de la fraction
+    @Override
+    public String toString() {
+        return "Je suis une fraction";
+    }
 
 }
--- SimpleFraction/src/SimpleFraction/Main.java
+++ SimpleFraction/src/SimpleFraction/Main.java
@@ -3,8 +3,15 @@ package SimpleFraction;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Bonjour");
+		
+		// Activer les assertions
+        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+
+     // Test de création de fractions
+        Fraction f1 = new Fraction(1, 2); // 1/2
+        Fraction f2 = new Fraction(3, 4); // 3/4
+//System.out.println(f1);
+assert f1.toString().equals("Je suis une fraction") : "Erreur c'est n'est pas une fraction";
 	}
 
 }
